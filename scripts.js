87b6cfe3725557d7d770db6226891e3e17273d48
Husayn <khaybabz@gmail.com>
2024-09-23 09:09:05 +0100

    added media query

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const home = document.getElementById('menu')
+const homeList = document.getElementById('nav')
+const cancel = document.querySelector('fa-circle-xmark')
+
+menu.addEventListener('click', ()=>{
+    homeListList.style.display = "flex"
+}) 
+function cancelMenu() {
+    homeList.style.display = "none"
+}
+
+cancel.addEventListener("click", cancelMenu)
